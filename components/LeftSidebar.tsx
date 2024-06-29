838a502d53f17a55b641c1768b8fd8395008f544
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-06-29 09:46:15 +0530

    improved code

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -22,7 +22,7 @@ const LeftSidebar = () => {
                 const isActive = pathname === route || pathname.startsWith(`${route}/`)
 
                 return <Link href={route} key={label} className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start', {
-                    'bg-nav-focus border-r-4 border-orange-500':isActive
+                    'bg-nav-focus border-r-4 border-green-500':isActive
                 })
                     
                 }>
